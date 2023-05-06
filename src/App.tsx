cd49f57c06bf09f8f5b8eee1016c2255aad34793
Fatih Candan <mfatihcandan@gmail.com>
2023-05-06 22:55:06 +0300

    project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/pages/Login";
+import Home from "./components/pages/Home";
 
 function App() {
+  const userAuth = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Router>
+        <Routes>
+          <Route path="/" element={userAuth ? <Home /> : <Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </MantineProvider>
   );
 }
 
